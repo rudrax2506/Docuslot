a425886b16754e030345daebf70cccab6ba68fb3
rudrax2506 <rudraxrishu@gmail.com>
2025-03-25 17:40:16 +1030

    Creating the Navigation Bar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div>
+            <img src={assets.logo} alt="" />
+            <ul>
+                <NavLink>
+                    <li>HOME</li>
+                    <hr />
+                </NavLink>
+                <NavLink>
+                    <li>ALL DOCTORS</li>
+                    <hr />
+                </NavLink>
+                <NavLink>
+                    <li>ABOUT</li>
+                    <hr />
+                </NavLink>
+                <NavLink>
+                    <li>CONTACT</li>
+                    <hr />
+                </NavLink>
+            </ul>
+            <div>
+                <button>Create Account</button>
+            </div>
+
+
+        </div >
+    )
+}
+
+export default Navbar
