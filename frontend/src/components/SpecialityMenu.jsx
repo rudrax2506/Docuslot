2749fa4af39c48ba508bd9a779a754cf7cb3235e
rudrax2506 <115683707+rudrax2506@users.noreply.github.com>
2025-04-18 19:53:19 +0930

    Updating the frontend

---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SpecialityMenu = () => {
+  return (
+    <div id = 'speciality'>
+        <h1>Find by Speciality</h1>
+
+    </div>
+  )
+}
+
+export default SpecialityMenu
