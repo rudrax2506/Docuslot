2a73b9296fdf14089ddf9e8c4d597791061698a5
rudrax2506 <115683707+rudrax2506@users.noreply.github.com>
2025-04-25 20:25:59 +0930

    Setting Up the AppContext in Frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import AppContextProvider from './context/AppContext';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </BrowserRouter>
 )
