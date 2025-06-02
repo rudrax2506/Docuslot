464ef7dffbeadb9f6fa0e41e5b70741a769cc101
rudrax2506 <115683707+rudrax2506@users.noreply.github.com>
2025-06-02 17:02:27 +0930

    Working on the Appointment Logic

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -23,15 +23,40 @@ const Appointment = () => {
 
     // Getting Current date
     let today = new Date()
-    for(let i = 0 ; i<7; i++){
+    for (let i = 0; i < 7; i++) {
       // getting date with index
       let currentDate = new Date(today)
       currentDate.setDate(today.getDate() + i)
 
       // setting end time of the date with index
       let endTime = new Date()
-      endTime.setDate(today.getDate()+1)
-      endTime.setHours(21,0,0,0)
+      endTime.setDate(today.getDate() + i)
+      endTime.setHours(21, 0, 0, 0)
+
+      // setting Hours
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+      } else {
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+      }
+
+      let timeSlots = []
+
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+        //add slot to array
+        timeSlots.push(
+          {
+            dateTime: new Date(currentDate),
+            time: formattedTime
+          })
+        // Increment Time by 30 Minutes
+        currentDate.setMinutes(currentDate.getMinutes() + 30)
+      }
+      setDocSlots(prev => ([...prev, timeSlots]))
+
     }
   }
 
@@ -41,7 +66,12 @@ const Appointment = () => {
 
   useEffect(() => {
     getAvailableSlots()
-  },[docInfo] )
+  }, [docInfo])
+
+  useEffect(() => {
+    console.log(docSlots)
+
+  }, [docSlots])
 
   return docInfo && (
     <div>
@@ -52,7 +82,7 @@ const Appointment = () => {
         </div>
         <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
           {/*-----------Doc Info: name, degree, experience---------------*/}
-          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>{docInfo.name} 
+          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>{docInfo.name}
             <img className='w-5' src={assets.verified_icon} alt="" /></p>
           <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
             <p>{docInfo.degree} - {docInfo.speciality}</p>
@@ -60,7 +90,7 @@ const Appointment = () => {
           </div>
           <div>
             {/*-----------Doctor About---------------*/}
-            <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>About <img src = {assets.info_icon} alt= ""/></p>
+            <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>About <img src={assets.info_icon} alt="" /></p>
             <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
           </div>
           <p className='text-gray-500 font-medium mt-4'>
