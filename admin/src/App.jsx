7fc6d4eefa354d8eb044fb344631459fbe9ec768
rudrax2506 <115683707+rudrax2506@users.noreply.github.com>
2025-06-16 20:10:05 +0930

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/Login'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
-  return (
+
+  const {aToken} = useContext(AppContext)
+  return aToken ? (
     <div>
       <Login />
       <ToastContainer />
