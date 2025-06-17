0f053076e93f889fa28a22ca5ab9fd4444c56e63
rudrax2506 <115683707+rudrax2506@users.noreply.github.com>
2025-06-18 00:09:42 +0930

    Working on Backend to add the functionality of seeing all the doctors by admin

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../../context/AdminContext'
 
 const DoctorsList = () => {
+
+  const {doctors, aToken, getAllDoctors} = useContext(AdminContext)
   return (
     <div>
         DoctorsList
