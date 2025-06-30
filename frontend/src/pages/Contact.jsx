cce0e7b80b0fb2af4e8333dc866a939da1b2ee07
rudrax2506 <115683707+rudrax2506@users.noreply.github.com>
2025-06-30 23:17:10 +0930

    Small update

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
           <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
           <p className=' text-gray-500'>17/32 Elgar Ave <br /> Ingle Farm SA 5098</p>
           <p className=' text-gray-500'>Tel: 0424552808 <br /> Email: rudraxrishu@gmail.com</p>
-          <p className=' font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
+          <p className=' font-semibold text-lg text-gray-600'>CAREERS AT DOCUSLOT</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
